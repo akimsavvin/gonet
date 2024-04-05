79fafcc28368b8bd4a3c5fa194124d3e60334368
Akim <savvinakim@gmail.com>
2024-04-05 20:13:15 +0300

    feat(di): added validateConstructor function

--- constructor.go
+++ constructor.go
@@ -1,1 +1,18 @@
 package gonet
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func validateConstructor(constructor any) {
+	constructorType := reflect.TypeOf(constructor)
+
+	if constructorType.Kind() != reflect.Func {
+		panic(fmt.Sprintf("constructor %v is not a function", constructor))
+	}
+
+	if constructorType.NumOut() != 1 {
+		panic(fmt.Sprintf("%v constructor must have only one return argument", constructor))
+	}
+}
