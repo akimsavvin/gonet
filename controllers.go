3afc9c91c9609f0d9b03f58b1a2922e999c27d2c
Akim <savvinakim@gmail.com>
2024-04-05 19:20:26 +0300

    feat(di): added controllers

---
+++ controllers.go
@@ -0,0 +1,35 @@
+package gonet
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Controller interface {
+	Register(g *ApiGroup)
+}
+
+func AddController(constructor any) {
+	validateConstructor(constructor)
+	typ := reflect.TypeOf(constructor).Out(0)
+	ctrlTyp := reflect.TypeOf((*Controller)(nil)).Elem()
+	if !typ.Implements(ctrlTyp) {
+		panic(fmt.Sprintf("controller %s does not implement Constructor interface", typ.Name()))
+	}
+
+	addTypeProvider(typ, constructor, controllerPr, LifetimeSingleton)
+}
+
+func getControllers() []Controller {
+	ctrls := make([]Controller, 0)
+
+	for _, p := range providers {
+		c, ok := p.getInstance().Interface().(Controller)
+
+		if p.kind == controllerPr && ok {
+			ctrls = append(ctrls, c)
+		}
+	}
+
+	return ctrls
+}
