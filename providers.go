6179c558d28872ff2e7cd5d8aaaf6e7672229b02
Akim <savvinakim@gmail.com>
2024-04-05 19:20:56 +0300

    feat(di): added providers and made service a kind

--- providers.go
+++ providers.go
@@ -1,1 +1,92 @@
 package gonet
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Lifetime int
+
+const (
+	LifetimeSingleton          Lifetime = iota + 1
+	LifetimeRequest_DO_NOT_USE          = iota + 1
+	LifetimeTransient                   = iota + 1
+)
+
+type providerKind int
+
+const (
+	servicePr    providerKind = iota + 1
+	controllerPr              = iota + 1
+	valuePr                   = iota + 1
+	configPr                  = iota + 1
+)
+
+type provider struct {
+	kind providerKind
+	// typ: all providers have
+	typ reflect.Type
+	// value: singleton providers have
+	value       *reflect.Value
+	constructor reflect.Value
+	lifetime    Lifetime
+}
+
+func (p *provider) getInstance() reflect.Value {
+	if p.lifetime == LifetimeSingleton && p.value != nil {
+		return *p.value
+	}
+
+	depsCount := p.constructor.Type().NumIn()
+	deps := make([]reflect.Value, depsCount)
+
+	for i := 0; i < depsCount; i++ {
+		depP := getTypeProvider(p.constructor.Type().In(i))
+		if depP.kind == controllerPr {
+			panic(fmt.Sprintf("no provider found for type %s", depP.typ))
+		}
+
+		deps[i] = depP.getInstance()
+	}
+
+	instance := p.constructor.Call(deps)[0]
+
+	if p.lifetime == LifetimeSingleton {
+		p.value = &instance
+	}
+
+	return instance
+}
+
+var providers = make(map[reflect.Type]*provider)
+
+func addTypeProvider(typ reflect.Type, constructor any, kind providerKind, lifetime Lifetime) {
+	pvdr := &provider{
+		kind:        kind,
+		typ:         typ,
+		value:       nil,
+		constructor: reflect.ValueOf(constructor),
+		lifetime:    lifetime,
+	}
+
+	providers[typ] = pvdr
+}
+
+func addProvider[TType any](constructor any, kind providerKind, lifetime Lifetime) {
+	typ := reflect.TypeOf((*TType)(nil)).Elem()
+	addTypeProvider(typ, constructor, kind, lifetime)
+}
+
+func getTypeProvider(typ reflect.Type) *provider {
+	p := providers[typ]
+	if p == nil {
+		panic(fmt.Sprintf("not provider found for type %s", typ))
+	}
+
+	return p
+}
+
+func getProvider[TProvider any]() *provider {
+	typ := reflect.TypeOf((*TProvider)(nil)).Elem()
+	return getTypeProvider(typ)
+}
--- services.go
+++ services.go
@@ -2,101 +2,20 @@ package gonet
 
 import (
 	"fmt"
-	"reflect"
 )
 
-type Scope int
-
-const (
-	ScopeSingleton          Scope = iota + 1
-	ScopeRequest_DO_NOT_USE       = iota + 1
-	ScopeTransient                = iota + 1
-)
-
-type service struct {
-	instance    *reflect.Value
-	constructor reflect.Value
-	typ         reflect.Type
-	lifetime    Scope
+func AddService[TService any](constructor any, lifetime Lifetime) {
+	validateConstructor(constructor)
+	addProvider[TService](constructor, servicePr, lifetime)
 }
 
-func (s *service) getInstance() reflect.Value {
-	if s.lifetime == ScopeSingleton && s.instance != nil {
-		return *s.instance
-	}
+func GetService[TService any]() TService {
+	p := getProvider[TService]()
+	instance, ok := p.getInstance().Interface().(TService)
 
-	depsCount := s.constructor.Type().NumIn()
-	deps := make([]reflect.Value, depsCount)
-
-	for i := 0; i < depsCount; i++ {
-		deps[i] = getServiceUnsafe(s.constructor.Type().In(i))
-	}
-
-	instance := s.constructor.Call(deps)[0]
-
-	if s.lifetime == ScopeSingleton {
-		s.instance = &instance
+	if p.kind != servicePr || !ok {
+		panic(fmt.Sprintf("no service found for type: %v", p.typ))
 	}
 
 	return instance
-}
-
-var services = make(map[reflect.Type]*service)
-
-func AddService[TService any](constructor any, lifetime Scope) {
-	constructorType := reflect.TypeOf(constructor)
-
-	if constructorType.Kind() != reflect.Func {
-		panic(fmt.Sprintf("constructor %v is not a function", constructor))
-	}
-
-	if constructorType.NumOut() != 1 {
-		panic(fmt.Sprintf("%v constructor must have only one return argument", constructor))
-	}
-
-	serviceType := reflect.TypeOf((*TService)(nil)).Elem()
-
-	svc := &service{
-		instance:    nil,
-		constructor: reflect.ValueOf(constructor),
-		typ:         serviceType,
-		lifetime:    lifetime,
-	}
-
-	services[serviceType] = svc
-}
-
-func getServiceUnsafe(serviceType reflect.Type) reflect.Value {
-	if serviceType != nil {
-		if svc, ok := services[serviceType]; ok {
-			return svc.getInstance()
-		}
-	}
-
-	for _, v := range services {
-		instance := v.getInstance()
-
-		if ok := instance.Type().AssignableTo(serviceType); ok {
-			return instance
-		}
-	}
-
-	panic(fmt.Sprintf("failed to resolve service %v", serviceType))
-}
-
-func GetService[TService any]() (rservice TService) {
-	serviceType := reflect.TypeOf((*TService)(nil)).Elem()
-
-	if serviceType != nil {
-		svc := services[serviceType]
-		instance, ok := svc.getInstance().Interface().(TService)
-
-		if !ok {
-			panic(fmt.Sprintf("gotten service %v type does not satisfy interface %v", svc, serviceType))
-		}
-
-		return instance
-	}
-
-	panic("no service found for requested type")
 }
