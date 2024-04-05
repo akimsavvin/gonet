3340a71dd3ace0d28507a8fa0d7aab8ca1829e4e
Akim <savvinakim@gmail.com>
2024-04-05 22:40:17 +0300

    feat(di): added constructor return type validation

--- services.go
+++ services.go
@@ -2,10 +2,19 @@ package gonet
 
 import (
 	"fmt"
+	"reflect"
 )
 
 func AddService[TService any](constructor any, lifetime Lifetime) {
-	validateConstructor(constructor)
+	validateConstructor[TService](constructor)
+
+	returnType := reflect.TypeOf(constructor).Out(0)
+	typ := reflect.TypeOf((*TService)(nil)).Elem()
+
+	if !typ.Out(0).Implements(typ) {
+		panic(fmt.Sprintf("constructor return type %v does not implement %v", returnType, typ))
+	}
+
 	addProvider[TService](constructor, servicePr, lifetime)
 }
 
