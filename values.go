a85ef77d75e959027887e508b5686e7b5affe624
Akim <savvinakim@gmail.com>
2024-04-17 16:40:57 +0300

    fix(values): AddValue not adds value via reflection instead of generic

--- values.go
+++ values.go
@@ -1,18 +1,22 @@
+// 🔥 GoNet is the first full-fledged framework made for Golang!
+// ⚡️ GoNet is inspired by .NET, NestJS and other languages frameworks
+// 🤖 GitHub Repository: https://github.com/akimsavvin/gonet
+
 package gonet
 
 import (
 	"reflect"
 )
 
-func AddValue[TValue any](value TValue) {
-	valTyp := reflect.TypeOf((*TValue)(nil)).Elem()
+func AddValue(value any) {
 	val := reflect.ValueOf(value)
+	valTyp := val.Type()
 
 	p := &provider{
 		typ:    valuePr,
 		valTyp: valTyp,
 		value:  &val,
-		constructor: reflect.ValueOf(func() TValue {
+		constructor: reflect.ValueOf(func() any {
 			return value
 		}),
 		lifetime: singleton,
