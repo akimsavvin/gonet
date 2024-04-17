59b900d2843fc9d7d23068a33ffd163c972699ce
Akim <savvinakim@gmail.com>
2024-04-17 16:37:15 +0300

    chore(design): added inspiration description

--- env.go
+++ env.go
@@ -1,3 +1,7 @@
+// 🔥 GoNet is the first full-fledged framework made for Golang!
+// ⚡️ GoNet is inspired by .NET, NestJS and other languages frameworks
+// 🤖 GitHub Repository: https://github.com/akimsavvin/gonet
+
 package gonet
 
 import "os"
