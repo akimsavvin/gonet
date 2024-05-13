c47e94e9177d27ffb7e20fcc26224dfeb937e1dc
Akim <savvinakim@gmail.com>
2024-05-14 01:44:50 +0700

    refactor(env): created env package

---
+++ env/env.go
@@ -0,0 +1,30 @@
+// 🔥 GoNet is the first full-fledged framework made for Golang!
+// ⚡️ GoNet is inspired by .NET, NestJS and other languages frameworks
+// 🤖 GitHub Repository: https://github.com/akimsavvin/gonet
+
+package env
+
+import "os"
+
+const (
+	Development = "Development"
+	Staging     = "Staging"
+	Production  = "Production"
+)
+
+func Current() (string, bool) {
+	env := os.Getenv("GONET_ENVIRONMENT")
+	ok := env != ""
+
+	return env, ok
+}
+
+func CurrentOrDefault() string {
+	env, ok := Current()
+
+	if !ok {
+		return Development
+	}
+
+	return env
+}
