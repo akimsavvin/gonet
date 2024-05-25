159bc7fb1a64b069aec4c9c41d31c592532e11cd
Akim <savvinakim@gmail.com>
2024-05-25 23:28:52 +0700

    fix(routing): got rin of copying http package

--- routing/router.go
+++ routing/router.go
@@ -1,6 +1,7 @@
 package routing
 
 import (
+	"net/http"
 	"strings"
 )
 
@@ -15,7 +16,7 @@ func newRouter() Router {
 	return &router{}
 }
 
-func (r *router) handlePattern(method Method, pattern string) string {
+func (r *router) handlePattern(method string, pattern string) string {
 	b := strings.Builder{}
 
 	b.WriteString(string(method))
@@ -27,7 +28,7 @@ func (r *router) handlePattern(method Method, pattern string) string {
 	return b.String()
 }
 
-func (r *router) handle(method Method, pattern string, handler HandlerFunc, middlewares ...Middleware) {
+func (r *router) handle(method string, pattern string, handler HandlerFunc, middlewares ...Middleware) {
 	pattern = r.handlePattern(method, pattern)
 	// TODO: handle pattern somehow
 	panic("not implemented")
@@ -42,37 +43,37 @@ func (r *router) SetPrefix(prefix string) {
 }
 
 func (r *router) GET(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodGet, pattern, handler, middlewares...)
+	r.handle(http.MethodGet, pattern, handler, middlewares...)
 }
 
 func (r *router) HEAD(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodHead, pattern, handler, middlewares...)
+	r.handle(http.MethodHead, pattern, handler, middlewares...)
 }
 
 func (r *router) POST(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodPost, pattern, handler, middlewares...)
+	r.handle(http.MethodPost, pattern, handler, middlewares...)
 }
 
 func (r *router) PUT(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodPut, pattern, handler, middlewares...)
+	r.handle(http.MethodPut, pattern, handler, middlewares...)
 }
 
 func (r *router) PATCH(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodPatch, pattern, handler, middlewares...)
+	r.handle(http.MethodPatch, pattern, handler, middlewares...)
 }
 
 func (r *router) DELETE(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodDelete, pattern, handler, middlewares...)
+	r.handle(http.MethodDelete, pattern, handler, middlewares...)
 }
 
 func (r *router) CONNECT(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodConnect, pattern, handler, middlewares...)
+	r.handle(http.MethodConnect, pattern, handler, middlewares...)
 }
 
 func (r *router) OPTIONS(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodOptions, pattern, handler, middlewares...)
+	r.handle(http.MethodOptions, pattern, handler, middlewares...)
 }
 
 func (r *router) TRACE(pattern string, handler HandlerFunc, middlewares ...Middleware) {
-	r.handle(MethodTrace, pattern, handler, middlewares...)
+	r.handle(http.MethodTrace, pattern, handler, middlewares...)
 }
