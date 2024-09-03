601d5b8c18f8858b1f041974ba405b205abd38fb
Akim <savvinakim@gmail.com>
2024-09-03 16:06:25 +0700

    SV-85: context removed from graceful.ShutdownFunc

--- graceful/shutdown.go
+++ graceful/shutdown.go
@@ -8,22 +8,15 @@ import (
 	"context"
 	"os"
 	"os/signal"
-	"time"
 )
 
-// ShutdownFunc describes a callback function that will be invoked on shutdown
-type ShutdownFunc = func(ctx context.Context)
-
 // OnShutdown block invoking goroutine and wait for os.Interrupt or os.Kill signals to invoke a callback
-func OnShutdown(callback ShutdownFunc) {
+func OnShutdown(callback func()) {
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt, os.Kill)
 	<-stop
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	callback(ctx)
+	callback()
 }
 
 // Context creates a new context cancelled on os.Interrupt or os.Kill signals
